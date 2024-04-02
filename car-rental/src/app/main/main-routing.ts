5157bb74b325798bc827acbc400c58e4a78acc46
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-04-02 18:40:31 +0300

    add routing

--- car-rental/src/app/main/main-routing.ts
+++ car-rental/src/app/main/main-routing.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
   { path: 'register/login',  component: LoginComponent}, 
+  { path: 'login/register', component: RegisterComponent}
 ];
 
 @NgModule({
